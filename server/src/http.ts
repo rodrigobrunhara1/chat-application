81dfa5f293076e8ab8ebd8da4c62a42aa90da6a7
Rodrigo Brunhara <rodrigo.brunhara@hotmail.com>
2023-10-01 21:15:59 -0300

    Add cors

--- server/src/http.ts
+++ server/src/http.ts
@@ -11,7 +11,7 @@ const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
     cors: {
-        origin: "*",
+        origin: "https://chat-application-frontend-five.vercel.app",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
