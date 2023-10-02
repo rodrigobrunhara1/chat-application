bfd808bef6f809d293238b604189fe92b7f01cd4
Rodrigo Brunhara <rodrigo.brunhara@hotmail.com>
2023-10-01 22:05:02 -0300

    Add cors

--- server/src/http.ts
+++ server/src/http.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors({
+    origin: ["https://chat-application-frontend-five.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}
+));
 app.use("/", (req, res) => {
     res.send("Server running! =D");
 })
+
+
 const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
